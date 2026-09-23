2f441d7fef281b24081f6d35581231ce7766bf72
salauddin <salauddin@hasan.com>
2026-09-23 12:47:29 +0600

    Add getDayType function for Question 2

--- answer.js
+++ answer.js
@@ -4,3 +4,22 @@ function describeValue(value) {
   let truthyOrFalsy = value ? "truthy" : "falsy";
   return type + " | " + truthyOrFalsy;
 }
+
+ 
+//Question No. 2
+function getDayType(day) {
+  let lowerDay = day.toLowerCase();
+  switch (lowerDay) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
