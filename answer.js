e053deffdcf21131fe85a56a30bd914482d9eff3
salauddin <salauddin@hasan.com>
2026-09-23 12:55:09 +0600

    Add validateUsername function for Question 3

--- answer.js
+++ answer.js
@@ -1,11 +1,10 @@
- //Question No. 1
+//Question No. 1
 function describeValue(value) {
   let type = typeof value;
   let truthyOrFalsy = value ? "truthy" : "falsy";
   return type + " | " + truthyOrFalsy;
 }
 
- 
 //Question No. 2
 function getDayType(day) {
   let lowerDay = day.toLowerCase();
@@ -23,3 +22,19 @@ function getDayType(day) {
       return "Invalid Day";
   }
 }
+
+//Question No. 3
+ 
+function validateUsername(username) {
+  if (username.length < 4) {
+    return "Too Short";
+  } else if (username.includes(" ")) {
+    return "No Space Allowed";
+  } else if (username.toLowerCase().includes("admin")) {
+    return "Reserved Word";
+  } else {
+    return "Available";
+  }
+}
+
+ 
