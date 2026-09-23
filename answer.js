9825ea5dbc1f44e1338a61fb3585581c6e365663
salauddin <salauddin@hasan.com>
2026-09-24 05:47:13 +0600

    Add getCngFare function for Question 4

--- answer.js
+++ answer.js
@@ -38,3 +38,23 @@ function validateUsername(username) {
 }
 
  
+//Question No. 4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let fare = 50;  
+  if (distance > 2) {
+    fare = fare + (distance - 2) * 15;
+  }
+  fare = fare + waitingMinutes * 2;
+  if (isNight) {
+    fare = fare + fare * 0.2;
+  }
+  return fare;
+}
+
+console.log(getCngFare(2));              // 50
+console.log(getCngFare(1));              // 50
+console.log(getCngFare(5));              // 95
+console.log(getCngFare(10));             // 170
+console.log(getCngFare(5, false, 10));   // 115
+console.log(getCngFare(5, true));        // 114
+console.log(getCngFare(5, true, 10));    // 138
